d53720b38148f8a92b9e8a21f19ad6b37bc1695c
gamerjamer43 <gamerjamer43@gmail.com>
2025-09-09 01:13:33 -0400

    complete main rework: private scandir function (in case i want to reuse), more info logged

--- ntest/core.py
+++ ntest/core.py
@@ -1,40 +1,64 @@
 from importlib.util import module_from_spec, spec_from_file_location
-from os import listdir
-from os.path import isfile, splitext
-from sys import platform
 from inspect import getmembers, isfunction
+from os import PathLike, listdir
+from os.path import isdir, isfile, join, splitext
+from platform import machine, node, python_compiler, python_version, system
+from shutil import get_terminal_size
+
+# version info
+from ntest import __version__
 
 # colorize func unused but i might, it's in printc tho
-from .colorize import Color, colorize, printc
+from .colorize import Color
 
+
+def _scandir(path: str | PathLike[str]) -> dict[str, list]:
+    # root directory to scan
+    root = path or '.'
+
+    # dict of test file paths and their lists of functions
+    tests: dict[str, list] = {}
+
+    # walk directory
+    for entry in listdir(root):
+        # get full path by joining current dir and current item
+        full_path = join(root, entry)
+        if isdir(full_path):
+            # recurse into subdirectories
+            tests.update(_scandir(full_path))
+
+        elif isfile(full_path):
+            name, ext = splitext(entry)
+            if ext == ".py" and name.endswith("_test"):
+                # load and exec test module
+                spec = spec_from_file_location(name, full_path)
+                module = module_from_spec(spec)
+                spec.loader.exec_module(module)
+
+                # gather all functions defined in that module
+                funcs = [
+                    func for _, func in getmembers(module, isfunction)
+                    if func.__module__ == name
+                ]
+                tests[full_path] = funcs
+
+    return tests
 
 def main() -> None:
-    for f in listdir('.'):
-        # print file, if its in dir... adding subdirectory checks soon but i can't rn it's 2 am
-        print(f, isfile(f))
+    # get terminal width
+    cols = get_terminal_size(fallback=(80, 24)).columns - 1
 
-        # this is where subdirectory checks are gonna go
-        if not isfile(f):
-            continue
+    # scan directory for tests
+    files: dict[str, list] = _scandir('.')
 
-        # if it is a file lets a go!
-        name, ext = splitext(f)
-        if ext == ".py" and name.endswith("_test"):
-            print("found a test!")
+    # print header info (gone add more as plugin manager and shit work in)
+    print(f"{Color.GREEN}{'=' * (cols // 2 - 6)} TESTS START {'=' * (cols // 2 - 6)}{Color.RESET}")
+    print(f"Platform: {Color.GREEN}{system()} {machine()}{Color.RESET}, Device Name: {Color.GREEN}{node()}{Color.RESET}")
+    print(f"Using: {Color.GREEN}Python {python_version()} [{python_compiler()}]{Color.RESET}")
+    print(f"Package Version: {Color.GREEN}ntest v{__version__}{Color.RESET}")
 
-            # load and exec test module
-            module_name = name
-            spec = spec_from_file_location(module_name, f)
-            module = module_from_spec(spec)
-            spec.loader.exec_module(module)
+    # go thru files and print functions (we're gonna call them for the tests)
+    for file, funcs in files.items():
+        print(f"Found file: {Color.GREEN}{file}{Color.RESET}, tests inside: {Color.GREEN}{', '.join(func.__name__ for func in funcs)}{Color.RESET}")
 
-            # gather all functions
-            functions = [
-                func for _, func in getmembers(module, isfunction)
-                if func.__module__ == module_name
-            ]
-
-            # print dem jawns out. this all i can do frn i need sleep
-            print(f"Functions: {", ".join(func.__name__ for func in functions)}")
-    
-    printc("\nPlatform:", Color.GREEN, platform, Color.RESET)
+    print(f"{Color.GREEN}{'=' * cols}{Color.RESET}")
---
+++ test2_test.py
@@ -0,0 +1,2 @@
+def test_other_test():
+    assert "test" == "test"
